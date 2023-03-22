7d90e845f6413b9132da1b1191820fa87014a44c
Max Smirnov <smirnovmax01@gmail.com>
2023-03-22 21:28:45 +0300

    step 5

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,9 +9,16 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('genDiff', () => {
+test('genDiff json/json', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
+  const result = readFile('expectedStylishFile.txt');
+  expect(genDiff(filepath1, filepath2)).toEqual(result);
+});
+
+test('genDiff yaml/yml', () => {
+  const filepath1 = getFixturePath('file1.yaml');
+  const filepath2 = getFixturePath('file2.yml');
   const result = readFile('expectedStylishFile.txt');
   expect(genDiff(filepath1, filepath2)).toEqual(result);
 });
--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import fs from 'fs';
 import path from 'path';
 import buildTree from './buildTree.js';
 import getFormatterContent from './formatters/stylish.js';
+import parse from './parsers.js';
 
 const genDiff = (filepath1, filepath2) => {
   const file1Data = fs.readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8');
   const file2Data = fs.readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8');
 
-  const obj1 = JSON.parse(file1Data);
-  const obj2 = JSON.parse(file2Data);
+  const format1 = path.extname(filepath1);
+  const format2 = path.extname(filepath2);
+
+  const obj1 = parse(file1Data, format1);
+  const obj2 = parse(file2Data, format2);
 
   const diffTree = buildTree(obj1, obj2);
   const str = getFormatterContent(diffTree);
